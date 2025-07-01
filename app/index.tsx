578b68b24fc88696756b7c88d1df94ce21c97b59
antonellavalle <antonelladla.valle@gmail.com>
2025-07-01 00:45:51 -0300

    Cambio en Index

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import { HomeScreen } from "@/src/screens/home/HomeScreen";
 
-export const screenOptions = {
-    headerShown: false,
-};
+// export const screenOptions = {
+//     headerShown: false,
+// };
 
 export default function IndexRoute() {
     return <HomeScreen />;
